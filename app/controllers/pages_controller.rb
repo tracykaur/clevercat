c85c7d739e50f786e5ccbb9d5ac45b601e6b20c4
tracykaur <tracykaur@yahoo.com>
2017-10-31 16:18:33 +1000

    added page links

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,6 +3,12 @@ class PagesController < ApplicationController
   def index
   end
 
+  def tutor_signup
+  end
+
+  def find_a_tutor
+  end
+
 
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'pages#index'
+
+  get '/tutor_signup', to: 'pages#tutor_signup'
+  get '/find_a_tutor', to: 'pages#find_a_tutor'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
