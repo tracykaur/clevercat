64d41a6f286490782a26b33ce39576128f293691
tracykaur <tracykaur@yahoo.com>
2017-11-02 09:22:38 +1000

    added paperclip

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,3 +13,9 @@
 //= require rails-ujs
 //= require turbolinks
 //= require_tree .
+var toggleNavigationButton = $('#nav-toggle'),
+    navigation = $('.nav');
+
+toggleNavigationButton.click(function(){
+    navigation.toggleClass('is-open');
+});
