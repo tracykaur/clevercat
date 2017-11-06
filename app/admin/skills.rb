716f6626288550b883d56336fd03a0b3172b6425
tracykaur <tracykaur@yahoo.com>
2017-11-07 09:37:52 +1000

    fixed active admin

--- app/admin/skills.rb
+++ app/admin/skills.rb
@@ -1,4 +1,5 @@
 ActiveAdmin.register Skill do
+  permit_params :name 
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
