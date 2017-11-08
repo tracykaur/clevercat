aaf15bd038270fe9875e8ff52c89cc5c4f5931d8
tracykaur <tracykaur@yahoo.com>
2017-11-09 09:59:07 +1000

    fixed email address

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -72,6 +72,13 @@ Rails.application.configure do
   # config.active_job.queue_adapter     = :resque
   # config.active_job.queue_name_prefix = "clevercat_#{Rails.env}"
   config.action_mailer.perform_caching = false
+  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.delivery_method = :mailgun
+  config.action_mailer.mailgun_settings = {
+  api_key: ENV.fetch('MAILGUN_API_KEY'),
+
+  domain: ENV.fetch('MAILGUN_DOMAIN'),
+}
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
